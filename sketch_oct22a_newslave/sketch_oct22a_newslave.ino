412b6920442807b246d7add27c17c5ba19645701
Frank <frank.somhorst@solarboattwente.nl>
2018-10-23 16:54:39 +0200

    niw

--- sketch_oct22a_newslave/sketch_oct22a_newslave.ino
+++ sketch_oct22a_newslave/sketch_oct22a_newslave.ino
@@ -2,35 +2,51 @@
 
 
 #include <Wire.h>
+#include <math.h>
+
+int Vsens = 10;
 
 void setup() {
   pinMode(9, INPUT_PULLUP);
   pinMode(10, INPUT_PULLUP);
   pinMode(11, INPUT_PULLUP);
   pinMode(12, INPUT_PULLUP);
+  pinMode(2, INPUT);
+  pinMode(A7, INPUT);
+  pinMode(A1, INPUT);
   delay(20);
 
   int address = 127 - (digitalRead(9)*8 + digitalRead(10)*4 + digitalRead(11)*2 + digitalRead(12)*1); // 111 xxxx als adres
   
   Wire.begin(address);
+
   
   Wire.onReceive(receiveEvent); // register event
+  Wire.onRequest(requestEvent);
+  
   pinMode(LED_BUILTIN, OUTPUT);           // LED
+  
+  Serial.begin(9600);
+  
 }
 
 void loop() {
-  ;
+ ;
 }
 
 // function that executes whenever data is received from master
 // this function is registered as an event, see setup()
 void receiveEvent() {
+   int x = B00000000;
+   int y = B00000000;
+   
   while (1 < Wire.available()) { // loop through all but the last
+    x = Wire.read();    // receive byte as an integer
+    y = Wire.read(); 
   }
-  int x = Wire.read();    // receive byte as an integer
+  //int x = Wire.read();    // receive byte as an integer
 
-
-  if (x == 0)
+  if (x == B00000000)
   {
     digitalWrite(LED_BUILTIN, HIGH);   
     delay(200);                      
@@ -38,4 +54,31 @@ void receiveEvent() {
     delay(200);
   }
   
+  if (bitRead(y,7) == 1) //(y == B10000001)
+  { 
+    digitalWrite(2, HIGH);
+  }
+}
+
+
+void requestEvent() {
+ int Vsens1 = analogRead(A7);
+ //Serial.print(Vsens);
+// Serial.print("\t");
+ //float testVsens = (1.725+1.850)/(1.725)*Vsens*(5.0/1023.0);
+ //Serial.print(testVsens);
+ //Serial.print("\t");
+ Vsens1 = Vsens1 >> 2;
+ //Serial.print(Vsens);
+ //testVsens = (1.725+1.850)/(1.725)*Vsens*(5.0/255.0);
+ //Serial.print("\t");
+ //Serial.print(testVsens);
+ //Serial.print("\n");
+ //delay(100);
+ Wire.write(Vsens1);
+ int Vsens2 = analogRead(A1);
+ Vsens2 = Vsens2 >> 2;
+ Wire.write(Vsens2);
+  
+
 }
--- sketch_oct22b_newmaster/sketch_oct22b_newmaster.ino
+++ sketch_oct22b_newmaster/sketch_oct22b_newmaster.ino
@@ -8,7 +8,7 @@ int x = 0;
 void setup() {
   Wire.begin(); // join i2c bus (address optional for master)
   Serial.begin(9600);
-  Serial.println(slaveScanner());
+  //Serial.println(slaveScanner());
 }
 
 word slaveScanner(){
@@ -26,7 +26,7 @@ word slaveScanner(){
  
     if (error == 0)
     {
-      slave += 1 << (address -112); 
+      slave += 1 << (address -112);  // dit doet die 0000 0000 0000 0000 1'tjes zetten om aan te geven of apparaat 16 t/m 1 er is. 
     }
   }
 
@@ -37,17 +37,50 @@ word slaveScanner(){
 
 void loop() {
   
-  x = 0;
-  
-  Wire.beginTransmission(BASE +1); // transmit to device #8
-  Wire.write(x);              // sends one byte
-  Wire.endTransmission();    // stop transmitting
-  Wire.beginTransmission((BASE+2));
-  Wire.write(x);
-  Wire.endTransmission();
+  //int x = B00000000;
+  //int y = B10000001;
+  //int x = B11111111;
+
+
+  //Wire.beginTransmission(BASE +1); // transmit to device 
+  //Wire.write(x);              // sends one byte    
+  //Wire.write(y);
+  //Wire.endTransmission();    // stop transmitting
+  //Wire.beginTransmission((BASE+2));
+  //Wire.write(x);
+  //Wire.write(y);
+  //Wire.endTransmission();
+
+  Wire.requestFrom(BASE + 1, 1);
+    while (Wire.available()) {
+      Serial.print("Device1= ");
+      Serial.print("\t");
+      int value = Wire.read();
+      Serial.print(value);
+      //Serial.print("\t");
+      //float Vsens = value*(5.0/255.0)*(3900.0/(3900.0+4700.0));
+      //Serial.print(Vsens);
+      Serial.print("\n");
+    }
 
   delay(100);
-  
+
+  Wire.requestFrom(BASE + 2, 2);
+    while (Wire.available()) {
+      Serial.print("Device2= ");
+      Serial.print("\t");
+      int value = Wire.read();
+      Serial.print(value);
+      int value2 = Wire.read();
+      Serial.print("\t");
+      Serial.print(value2); 
+      //Serial.print("\t");
+      //float Vsens = value*(5.0/255.0)*(3900.0/(3900.0+4700.0));
+      //Serial.print(Vsens);
+      Serial.print("\n");
+    }
+
+  delay(500);
 
   
 }
